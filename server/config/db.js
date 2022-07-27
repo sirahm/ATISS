efa06fa2cbba0cea4df8dcbc40774ece79af6c96
Tabriz123 <95578396+Tabriz123@users.noreply.github.com>
2022-07-27 18:04:07 -0500

    Delete server directory

--- server/config/db.js
+++
@@ -1,6 +0,0 @@
-//connecting to the mongodb survey_answer database
-module.exports = 
-{
-    "URI": "mongodb+srv://atiss:LbGnvmBQSqw8vaVf@cluster0.htmvx.mongodb.net/survey_answer?retryWrites=true&w=majority",
-    "Secret": 'SomeSecret'
-}
