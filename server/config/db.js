49bbd992c0026775789b911febaaf74c56f194dc
Tabriz123 <95578396+Tabriz123@users.noreply.github.com>
2022-07-27 17:50:03 -0500

    Updated db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 //connecting to the mongodb survey_answer database
 module.exports = 
 {
-    "URI": "mongodb+srv://atiss:LbGnvmBQSqw8vaVf@cluster0.htmvx.mongodb.net/survey_answer?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://atiss:LbGnvmBQSqw8vaVf@cluster0.htmvx.mongodb.net/survey_answer?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
 }
